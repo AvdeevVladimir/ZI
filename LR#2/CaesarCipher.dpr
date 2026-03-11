3df5853a9fcbc022f0d6614287ce7392c0c2dea5
AvdeevVladimir <ballisto0@gmail.com>
2026-03-11 13:00:42 +1000

    Add files via upload

--- LR#2/CaesarCipher.dpr
+++ LR#2/CaesarCipher.dpr
@@ -1,17 +1,14 @@
 ﻿program DoubleSquareCipherEnglish;
-
+
 {$APPTYPE CONSOLE}
 
 uses
   SysUtils, Windows;
 
 const
-  // Английский алфавит (26 букв, но для квадрата 5x5 нужно 25)
-  // Убираем одну букву, например 'J'
   ENG_ALPHABET = 'ABCDEFGHIKLMNOPQRSTUVWXYZ';
   ALPH_SIZE = 25;
 
-// Функция для удаления неподходящих символов и приведения к верхнему регистру
 function PrepareText(const Input: string): string;
 var
   i: Integer;
@@ -22,17 +19,14 @@ begin
 
   for i := 1 to Length(UpperStr) do
   begin
-    // Оставляем только буквы из алфавита
     if Pos(UpperStr[i], ENG_ALPHABET) > 0 then
       Result := Result + UpperStr[i];
   end;
 
-  // Если длина нечетная, добавляем 'Х' (чтобы было четное количество)
   if Odd(Length(Result)) then
-    Result := Result + 'Х';
+    Result := Result + 'X';  // Исправлено: 'Х' -> 'X' (латиница)
 end;
 
-// Построение квадрата по ключевому слову
 function BuildSquare(const Keyword: string): string;
 var
   i: Integer;
@@ -41,7 +35,6 @@ begin
   Result := '';
   FillChar(Used, SizeOf(Used), 0);
 
-  // Сначала добавляем уникальные буквы ключа
   for i := 1 to Length(Keyword) do
   begin
     if (Pos(Keyword[i], ENG_ALPHABET) > 0) and (not Used[Ord(Keyword[i])]) then
@@ -51,7 +44,6 @@ begin
     end;
   end;
 
-  // Затем добавляем остальные буквы алфавита
   for i := 1 to ALPH_SIZE do
   begin
     if not Used[Ord(ENG_ALPHABET[i])] then
@@ -59,7 +51,6 @@ begin
   end;
 end;
 
-// Поиск координат буквы в квадрате
 procedure FindPosition(const Square: string; Ch: Char; var Row, Col: Integer);
 var
   PosInSquare: Integer;
@@ -78,7 +69,6 @@ begin
   end;
 end;
 
-// Функция шифрования
 function Encrypt(const PlainText, Key1, Key2: string): string;
 var
   Square1, Square2: string;
@@ -86,35 +76,33 @@ var
   r1, c1, r2, c2: Integer;
   PreparedText: string;
 begin
-  // Подготовка текста и квадратов
   PreparedText := PrepareText(PlainText);
   Square1 := BuildSquare(Key1);
   Square2 := BuildSquare(Key2);
 
   Result := '';
 
-  // Обрабатываем по парам (только четное количество символов)
   i := 1;
   while i < Length(PreparedText) do
   begin
-    // Находим координаты в квадратах
     FindPosition(Square1, PreparedText[i], r1, c1);
     FindPosition(Square2, PreparedText[i+1], r2, c2);
 
-    // Шифрование по правилу: (r1,c2) из первого квадрата и (r2,c1) из второго
+    // Берем букву из первого квадрата с координатами (r1, c2)
     Result := Result + Square1[r1 * 5 + c2 + 1];
+    // Берем букву из второго квадрата с координатами (r2, c1)
     Result := Result + Square2[r2 * 5 + c1 + 1];
 
     Inc(i, 2);
   end;
 end;
 
-// Функция дешифрования
 function Decrypt(const CipherText, Key1, Key2: string): string;
 var
   Square1, Square2: string;
   i: Integer;
   r1, c1, r2, c2: Integer;
+  Ch1, Ch2: Char;
 begin
   Square1 := BuildSquare(Key1);
   Square2 := BuildSquare(Key2);
@@ -124,19 +112,23 @@ begin
   i := 1;
   while i < Length(CipherText) do
   begin
-    // Находим координаты в квадратах
-    FindPosition(Square1, CipherText[i], r1, c1);
-    FindPosition(Square2, CipherText[i+1], r2, c2);
+    Ch1 := CipherText[i];
+    Ch2 := CipherText[i+1];
 
-    // Дешифрование - обратный процесс
+    // Находим позиции зашифрованных букв в соответствующих квадратах
+    FindPosition(Square1, Ch1, r1, c1);  // первая буква - из первого квадрата
+    FindPosition(Square2, Ch2, r2, c2);  // вторая буква - из второго квадрата
+
+    // Для дешифровки используем обратные координаты:
+    // Исходная первая буква находится в первом квадрате на позиции (r1, c2)
     Result := Result + Square1[r1 * 5 + c2 + 1];
+    // Исходная вторая буква находится во втором квадрате на позиции (r2, c1)
     Result := Result + Square2[r2 * 5 + c1 + 1];
 
     Inc(i, 2);
   end;
 end;
 
-// Вспомогательная функция для вывода квадрата
 procedure PrintSquare(const Square: string; const Title: string);
 var
   i, j: Integer;
@@ -158,15 +150,13 @@ begin
   Writeln;
 end;
 
-// Основная программа
 var
   PlainText, CipherText, DecryptedText: string;
   Key1, Key2: string;
   PreparedOriginal: string;
-  i: Integer;  // Объявляем переменную для цикла
+  i: Integer;
 begin
   try
-    // Настройка консоли для русского языка
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
 
@@ -174,7 +164,6 @@ begin
     Writeln('================================');
     Writeln;
 
-    // Ввод ключей
     Write('Введите первое ключевое слово: ');
     Readln(Key1);
     Key1 := UpperCase(Key1);
@@ -184,26 +173,21 @@ begin
     Key2 := UpperCase(Key2);
     Writeln;
 
-    // Показываем сгенерированные квадраты
     PrintSquare(BuildSquare(Key1), 'Первый квадрат (ключ: ' + Key1 + '):');
     PrintSquare(BuildSquare(Key2), 'Второй квадрат (ключ: ' + Key2 + '):');
 
-    // Ввод текста для шифрования
     Write('Введите текст для шифрования: ');
     Readln(PlainText);
     Writeln;
 
-    // Шифрование
     CipherText := Encrypt(PlainText, Key1, Key2);
     Writeln('Зашифрованный текст: ', CipherText);
     Writeln;
 
-    // Дешифрование
     DecryptedText := Decrypt(CipherText, Key1, Key2);
     Writeln('Расшифрованный текст: ', DecryptedText);
     Writeln;
 
-    // Проверка
     PreparedOriginal := PrepareText(PlainText);
     if PreparedOriginal = DecryptedText then
     begin
@@ -215,7 +199,6 @@ begin
       Writeln('Оригинал (подготовленный): ', PreparedOriginal);
       Writeln('Расшифровка: ', DecryptedText);
 
-      // Дополнительная диагностика
       Writeln;
       Writeln('ДИАГНОСТИКА:');
       Writeln('Длина подготовленного оригинала: ', Length(PreparedOriginal));
@@ -240,3 +223,4 @@ begin
       Writeln('Ошибка: ', E.Message);
   end;
 end.
+
